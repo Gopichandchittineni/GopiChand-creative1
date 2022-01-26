90d0e5ffd7d38ab50a3c9519b9de4fe883baa217
Akash Kethavath <akashlucky2331@gmail.com>
2022-01-27 02:42:35 +0530

    final

---
+++ public/viewpage/welcome_message.js
@@ -0,0 +1,16 @@
+export const html = `
+
+<h1>Welcome to Awesome Discussion Forum</h1>
+Please Sign In To Do:
+<ul>
+    <li>TO Create a New Thread</li>
+    <li>TO Browse Thread</li>
+    <li>TO Reply Message to Thread</li>
+</ul>
+No Account Yet?
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#modal-create-account">
+Create Account
+</button>
+
+`;
